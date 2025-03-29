e8ad0406ec2def6df9fdcd281324af6dc32b52ae
Sharan <cmsharan.2019@gmail.com>
2025-03-29 20:21:26 +0530

    Week 10 react-routing

--- Week10/live/react-routing/src/Layout.jsx
+++ Week10/live/react-routing/src/Layout.jsx
@@ -1,3 +1,18 @@
+// import { Outlet } from "react-router-dom"
+// import { Header } from "./Header"
+
+// export const Layout = () => {
+//     return (
+//       <div style={{height: '100vh', background: 'green'}}>
+//         <Header />
+//       <div style={{height: '90vh', background: 'red' }}>
+//         <Outlet />
+//       </div>
+//       Footer | Contact Us
+//       </div>
+//     )
+// }
+
 import { Outlet } from "react-router-dom"
 import { Header } from "./Header"
 
