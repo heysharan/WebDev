837840a15f1ac65b2057dcc9db5b3a1e8b4c8572
Sharan <cmsharan.2019@gmail.com>
2025-04-06 22:43:44 +0530

    Week 11 react-recoil

--- Week-11/live/react-recoil/src/CountProvider.jsx
+++ Week-11/live/react-recoil/src/CountProvider.jsx
@@ -14,3 +14,27 @@ export const CountProvider = ({ children }) => {
     </countContext.Provider>
   );
 };
+
+
+
+
+
+
+
+
+
+// export const countContext = createContext();
+// export const CountProvider = ({ children }) => {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <countContext.Provider
+//       value={{
+//         count,
+//         setCount,
+//       }}
+//     >
+//       {children}
+//     </countContext.Provider>
+//   );
+// };
+
