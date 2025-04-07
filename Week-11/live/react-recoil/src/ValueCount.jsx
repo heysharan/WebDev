4e531f4833e31c5c10dbda5d12b74f5925ec66cb
Sharan <cmsharan.2019@gmail.com>
2025-04-07 21:32:50 +0530

    Week 11 react-recoil

--- Week-11/live/react-recoil/src/ValueCount.jsx
+++ Week-11/live/react-recoil/src/ValueCount.jsx
@@ -7,3 +7,19 @@ export const ValueCount = () => {
         <div>{`Count: ${count}`}</div>
     )
 }
+
+
+
+
+
+
+
+// import { useContext } from "react"
+// import { countContext } from "./CountProvider"
+
+// export const ValueCount = () => {
+//     const { count } = useContext(countContext);       {/* using the context api for state management */}
+//     return (
+//         <div>{`Count: ${count}`}</div>
+//     )
+// }
