d712e1e643bcb1ea140d4c4b6002d458b721331f
Sharan <cmsharan.2019@gmail.com>
2025-04-13 22:48:43 +0530

    Week 13 live react-tailwind

---
+++ Week-13/live/react-tailwind/components/LoginShadcn.jsx
@@ -0,0 +1,32 @@
+import { Button } from "./Buttons";
+
+export const LoginOtp = () => {
+
+  const isDisabled = true
+  return (
+    <div className="w-screen min-h-screen bg-blue-500">
+      <div className="flex justify-center pt-15 pb-20">
+        <img src="https://webinar.gg/loginLogo.svg" alt="Logo" />
+      </div>
+
+      <div className="flex justify-center pb-15">
+        <h1 className="text-white text-2xl font-bold">
+          Check Your Email For An OTP
+        </h1>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-2">
+        <p className="text-white text-sm">
+          Please enter the verification code sent to your email id
+        </p>
+
+      </div>
+      <div className="flex flex-col items-center justify-center pt-6 gap-1">
+        <Button isDisabled={isDisabled}>Verify</Button>
+        <p className="text-white text-sm">
+          Can't find the email? Click here to resend
+        </p>
+      </div>
+    </div>
+  );
+};
