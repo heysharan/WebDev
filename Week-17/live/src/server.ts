99137e87b70e57b0e6b194ce10672da0dc0e1019
Sharan <cmsharan.2019@gmail.com>
2025-05-01 21:52:01 +0530

    init

---
+++ Week-17/live/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello"
+    })
+})
+
+app.listen(3000, () => {
+    console.log(`PORT running on 3000`)
+})
