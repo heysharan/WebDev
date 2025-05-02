6b61328cfd7720a33d3c66df3315d3730272f19b
Sharan <cmsharan.2019@gmail.com>
2025-05-02 23:22:23 +0530

    init

--- Week-17/live/src/server.ts
+++ Week-17/live/src/server.ts
@@ -1,3 +1,18 @@
+// import express from 'express';
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: "Hello"
+//     })
+// })
+
+// app.listen(3000, () => {
+//     console.log(`PORT running on 3000`)
+// })
+
+
 import express from 'express';
 
 const app = express();
