f8fa4c5518f5b5dbeae1afbc5f3f82215d23423a
Sharan <cmsharan.2019@gmail.com>
2025-05-04 21:39:47 +0530

    init pg

--- Week-17/live/src/server.ts
+++ Week-17/live/src/server.ts
@@ -1,3 +1,29 @@
+// import { Client } from 'pg';
+// import express from 'express';
+
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_2qZtGEJ1NAjQ",
+//     port: 5432,
+//     host: "ep-gentle-snow-a4fgzbzm-pooler.us-east-1.aws.neon.tech",
+//     database: 'neondb',
+//     ssl: true
+// });
+
+// const main = async () => {
+//     await pgClient.connect();
+
+//     const response = await pgClient.query(`CREATE TABLE todos (
+//         id SERIAL PRIMARY key,
+//         title VARCHAR(50),
+//         description VARCHAR(100),
+//         done BOOLEAN
+//         );`)
+//     console.log(response.rows);
+// }
+
+// main();
+
 import { Client } from 'pg';
 import express from 'express';
 
@@ -25,4 +51,5 @@ const main = async () => {
 main();
 
 
+ 
  
