bdc80bda342eb0a14cf40e7d3f5cc33da74dfd50
Sharan <cmsharan.2019@gmail.com>
2025-05-06 22:03:53 +0530

    pg Joins

--- Week-17/live/src/server.ts
+++ Week-17/live/src/server.ts
@@ -138,6 +138,87 @@
 // main();
 
 
+// import { Client } from 'pg';
+// import express from 'express';
+
+// const app = express();
+// app.use(express.json());
+
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_2qZtGEJ1NAjQ",
+//     port: 5432,
+//     host: "ep-gentle-snow-a4fgzbzm-pooler.us-east-1.aws.neon.tech",
+//     database: 'neondb',
+//     ssl: true
+// });
+// pgClient.connect();
+
+// app.post('/signup', async (req, res) => {
+//     const { username, email, password, street, city, country, pincode } = req.body;
+
+//     try {
+//         const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;`
+//         const addressInsertQuery = `INSERT INTO address (street, city, country, password, userId) VALUES ($1, $2, $3, $4, $5)`
+
+//         await pgClient.query('BEGIN');
+//         const response = await pgClient.query(insertQuery, [username, email, password])
+//         const userId = response.rows[0].id;
+//         const addressResponse = await pgClient.query(addressInsertQuery, [street, city, country, pincode, userId])
+//         await pgClient.query('COMMIT');
+
+//         res.json({
+//             message: 'You have Sign Up!'
+//         })
+//     } catch (e) {
+//         res.json({
+//             message: `Error while Signing Up ${e}`
+//         })
+//     }
+
+// })
+
+// app.listen(3000)
+
+
+// // Un-optimal Solutions → sending two different request to get the data from DB
+// import { Client } from 'pg';
+// import express from 'express';
+
+// const app = express();
+// app.use(express.json());
+
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_2qZtGEJ1NAjQ",
+//     port: 5432,
+//     host: "ep-gentle-snow-a4fgzbzm-pooler.us-east-1.aws.neon.tech",
+//     database: 'neondb',
+//     ssl: true
+// });
+// pgClient.connect();
+
+// app.get('/metadata', async (req, res) => {
+
+//     const id = req.query.id;
+
+//     const query1 = `SELECT id, username, email FROM users WHERE id=$1`;
+//     const response1 = await pgClient.query(query1, [id])
+
+//     const query2 = `SELECT * FROM address WHERE userId=$1`;
+//     const response2 = await pgClient.query(query2, [id]);
+
+//     res.json({
+//         user: response1.rows[0],
+//         address: response2.rows
+//     })
+
+// })
+
+// app.listen(3000)
+
+
+
 import { Client } from 'pg';
 import express from 'express';
 
@@ -154,27 +235,17 @@ const pgClient = new Client({
 });
 pgClient.connect();
 
-app.post('/signup', async (req, res) => {
-    const { username, email, password, street, city, country, pincode } = req.body;
+app.get('/metadata', async (req, res) => {
 
-    try {
-        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;`
-        const addressInsertQuery = `INSERT INTO address (street, city, country, password, userId) VALUES ($1, $2, $3, $4, $5)`
+    const id = req.query.id;
 
-        await pgClient.query('BEGIN');
-        const response = await pgClient.query(insertQuery, [username, email, password])
-        const userId = response.rows[0].id;
-        const addressResponse = await pgClient.query(addressInsertQuery, [street, city, country, pincode, userId])
-        await pgClient.query('COMMIT');
+    const query = `SELECT u.id, u.username, u.email, a.street, a.city, a.country, a.pincode
+                    FROM users u FULL JOIN address a ON u.id = a.userId WHERE a.userId = $1;`
+    const response = await pgClient.query(query, [id])
 
-        res.json({
-            message: 'You have Sign Up!'
-        })
-    } catch (e) {
-        res.json({
-            message: `Error while Signing Up ${e}`
-        })
-    }
+    res.json({
+        response: response.rows[0]
+    })
 
 })
 
