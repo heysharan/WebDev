9ff813f353c45f5173e47b62822c15a461b9c994
Sharan <cmsharan.2019@gmail.com>
2025-05-18 22:09:40 +0530

    nextjs-part3

--- Week-19/live/nextjs-part3/src/app/blogs/[...blogIds]/page.tsx
+++ Week-19/live/nextjs-part3/src/app/blogs/[...blogIds]/page.tsx
@@ -1,3 +1,28 @@
+// import axios from 'axios'
+
+// export default async function Blogs({ params }: {params: { blogId: number}}) {
+
+//     const postId = (await params).blogId
+
+//     const blog: any = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    
+//     return(
+//         <div className='flex flex-col justify-center gap-2'>
+//             <p className='text-2xl mt-10'>Post Id - {postId}</p>
+//             <div className='flex'>
+//             <h1 className='text-bold text-xl'>Title - {blog.data.title}</h1>
+//             </div>
+//             <div className='flex'>
+//             <h2 className='text-bold text-xl'>Body - {blog.data.body}</h2>
+//             </div>
+
+//         </div>
+//     )
+// }
+
+
+
+
 import axios from 'axios'
 
 export default async function Blogs({ params }: {params: { blogId: number}}) {
