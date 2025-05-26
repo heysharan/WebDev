1d64c58f390327f213162f4cae7e10c3e3fc7652
Sharan <cmsharan.2019@gmail.com>
2025-05-26 18:26:12 +0530

    next-auth

--- Week-20/live/next-auth/src/app/api/auth/[...nextauth]/route.ts
+++ Week-20/live/next-auth/src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,64 @@
+import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
+import prismaClient from '../../../db'
+import argon2 from 'argon2';
+import GoogleProvider from "next-auth/providers/google";
 
-providers: [
-  CredentialsProvider({
-    name: "Credentials",
-    credentials: {
-      username: { label: "Username", type: "text", placeholder: "jsmith" },
-      password: { label: "Password", type: "password" }
-    },
-    async authorize(credentials, req) {
-      const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "someone@something.com" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
 
-      if (user) {
-        return user
-      } else {
-        return null
+        console.log(email, password)
+
+        const response = await prismaClient.user.findFirst({
+          where: {
+            email: email
+          }
+        })
+
+        type StrictUser = {
+          id: string;
+          email: string;
+          firstname: string;
+          lastname: string;
+        };
+
+        if (!response || !response.id || !response.email || !response.firstname || !response.lastname) {
+          throw new Error("Invalid user");
+        }
+
+        const strictUser: StrictUser = {
+          id: response.id.toString(),
+          email: response.email,
+          firstname: response.firstname,
+          lastname: response.lastname,
+        };
+
+
+        const verifiedUser = await argon2.verify(response?.password!, password!)
+
+        if (!verifiedUser) return null
+
+        return strictUser
 
       }
-    }
+    }),
+    GoogleProvider({
+    clientId: 'GOOGLE_CLIENT_ID',
+    clientSecret: 'GOOGLE_CLIENT_SECRET'
   })
-]
+  ],
+  pages: {
+    signIn: '/signin',
+  }
+})
+
+export { handler as GET, handler as POST }
