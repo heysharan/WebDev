912e39549c8abe80dbe3de26b6e7122c615708ed
Turbobot <turbobot@vercel.com>
2025-05-29 17:24:02 +0530

    feat(create-turbo): apply official-starter transform

--- Week-21/live/chat-app/apps/web/app/page.tsx
+++ Week-21/live/chat-app/apps/web/app/page.tsx
@@ -1,102 +1,35 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  return(
+    <div className='h-screen w-screen bg-black flex justify-center items-center'>
+            <div className='w-120 h-153 border border-gray-700 shadow-xl rounded-lg'>
+                <div className='m-5'>
+                    <div className='flex'>
+                        {/* <div className='flex items-center'><ChatIcon /></div> */}
+                        <p className=' text-white text-xl font-bold ml-2'>Real Time Chat</p>
+                    </div>
+                    <p className='text-gray-400 text-sm'>temporary room that expires after all users exit</p>
+                </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.com/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+                <div className='bg-gray-600 m-5 p-2 rounded-md text-gray-400 text-xs flex justify-between'>
+                    <p>Room Code:</p>
+                    <p>Users:</p>
+                </div>
+
+                <div className='h-96 border border-gray-700 shadow-xl rounded-lg m-5 overflow-y-auto p-4 flex flex-col gap-2'>
+                    {/* {messages.map((m, i) => (
+                        <div key={i} className='self-end bg-white text-black rounded-md px-4 py-2 max-w-xs text-right'>
+                            {m}
+                        </div>
+                    ))}
+                    <div ref={messageRef}></div> */}
+                </div>
+
+
+                <div className='flex m-5'>
+                    <input className='text-white pt-2 pr-3 pb-2 pl-3 mr-2 w-full focus:outline-none focus:ring-0 border border-gray-700 rounded-sm' type='text' placeholder='Type a message....'></input>
+                    <div className='bg-white rounded-sm flex items-center py-1 px-8 cursor-pointer'>Send</div>
+                </div>
+            </div>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turborepo.com?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turborepo.com →
-        </a>
-      </footer>
-    </div>
-  );
+  )
 }
