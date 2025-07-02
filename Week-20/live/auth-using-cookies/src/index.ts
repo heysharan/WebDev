b6bc23121d2cf1d7f7e9acbe848567fc82768f2d
Sharan <cmsharan.2019@gmail.com>
2025-07-02 23:26:31 +0530

    auth-using-cookies

--- Week-20/live/auth-using-cookies/src/index.ts
+++ Week-20/live/auth-using-cookies/src/index.ts
@@ -2,11 +2,19 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello",
-  });
+app.get("/signup", (req, res) => {
 });
+
+app.get("/signin", (req, res) => {
+});
+
+app.get("/user", (req, res) => {
+});
+
+app.get("/logout", (req, res) => {
+});
+
+
 
 app.listen(3000, () => {
     console.log('Server running on PORT 3000')
