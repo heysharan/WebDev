06d832a51ea605a5121754a6de62bc185150d91a
Sharan <cmsharan.2019@gmail.com>
2025-07-05 23:09:54 +0530

    next-auth

--- Week-20/live/next-auth/src/app/lib/auth.ts
+++ Week-20/live/next-auth/src/app/lib/auth.ts
@@ -1,26 +1,32 @@
 import CredentialsProvider from "next-auth/providers/credentials";
-import prismaClient from '../db'
-import argon2 from 'argon2';
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import prismaClient from "../db";
+import argon2 from "argon2";
 
 export const NEXT_AUTH = {
   providers: [
     CredentialsProvider({
       name: "Email",
       credentials: {
-        email: { label: "Email", type: "text", placeholder: "someone@something.com" },
-        password: { label: "Password", type: "password" }
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "someone@something.com",
+        },
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
         const email = credentials?.email;
         const password = credentials?.password;
 
-        console.log(email, password)
+        console.log(email, password);
 
         const response = await prismaClient.user.findFirst({
           where: {
-            email: email
-          }
-        })
+            email: email,
+          },
+        });
 
         type StrictUser = {
           id: string;
@@ -28,33 +34,48 @@ export const NEXT_AUTH = {
           name: string;
         };
 
-        if (!response || !response.id || !response.email || !response.firstname || !response.lastname) {
+        if (
+          !response ||
+          !response.id ||
+          !response.email ||
+          !response.firstname ||
+          !response.lastname
+        ) {
           throw new Error("Invalid user");
         }
 
         const strictUser: StrictUser = {
           id: response.id.toString(),
           email: response.email,
-          name: response.firstname
+          name: response.firstname,
         };
 
+        const verifiedUser = await argon2.verify(response.password!, password!);
 
-        const verifiedUser = await argon2.verify(response.password!, password!)
+        if (!verifiedUser) return null;
 
-        if (!verifiedUser) return null
-
-        return strictUser
-
-      }
-    })
+        return strictUser;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     session: ({ session, token, user }: any) => {
-      if(session && session.user){
+      if (session && session.user) {
         session.user.id = token.sub;
       }
-      return session
-    }
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/signin',
   }
-}
+};
--- Week-20/live/next-auth/src/app/signin/page.tsx
+++ Week-20/live/next-auth/src/app/signin/page.tsx
@@ -1,37 +1,77 @@
-'use client'
-import axios from 'axios'
-import { useRef } from 'react'
+"use client";
+import { signIn } from "next-auth/react";
+import { useRef } from "react";
 
 export default function Signin() {
-    const emailRef = useRef<HTMLInputElement>(null)
-    const passwordRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
-    const signin = async () => {
-        const res: any = await axios.post('http://localhost:3000/bad-approach-jwt-localstorage/api/signin', {
-            email: emailRef.current?.value,
-            password: passwordRef.current?.value
-        })
-        localStorage.setItem('token', res.data.token)
-    }
-    return (
-        <div className="w-screen h-screen flex justify-center items-center">
-            <div className="rounded-2xl w-120 h-92 border border-neutral-800">
-                <div className="flex flex-col p-5 gap-1">
-                    <p className="font-bold text-3xl text-white">Login</p>
-                    <p className="text-white">Enter your email below to login to your account</p>
-                </div>
-                <div className="flex flex-col pr-5 pb-5 pl-5">
-                    <p className="font-light text-lg text-white">Email</p>
-                    <input ref={emailRef} type="text" placeholder="someone@example.com" className="p-2 border border-neutral-800 rounded-lg outline-none"></input>
-                </div>
-                <div className="flex flex-col pr-5 pb-5 pl-5">
-                    <p className="font-light text-lg text-white">Password</p>
-                    <input ref={passwordRef} type="password" className="p-2 border border-neutral-800 rounded-lg outline-none"></input>
-                </div>
-                <div className="flex flex-col justify-center items-center pt-3 pr-5 pb-5 pl-5">
-                    <button className="bg-white text-black px-7 py-2 cursor-pointer rounded-md font-light flex justify-center items-center w-full" onClick={signin}>Login</button>
-                </div>
-            </div>
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="rounded-2xl w-120 h-130 border border-neutral-800">
+        <div className="flex flex-col p-5 gap-1">
+          <p className="font-bold text-3xl text-white">Login</p>
+          <p className="text-white">
+            Enter your email below to login to your account
+          </p>
         </div>
-    )
+        <div className="flex flex-col pr-5 pb-5 pl-5">
+          <p className="font-light text-lg text-white">Email</p>
+          <input
+            ref={emailRef}
+            type="text"
+            placeholder="someone@example.com"
+            className="p-2 border border-neutral-800 rounded-lg outline-none"
+          ></input>
+        </div>
+        <div className="flex flex-col pr-5 pb-5 pl-5">
+          <p className="font-light text-lg text-white">Password</p>
+          <input
+            ref={passwordRef}
+            type="password"
+            className="p-2 border border-neutral-800 rounded-lg outline-none"
+          ></input>
+        </div>
+        <div className="flex flex-col justify-center items-center pt-3 pr-5 pb-5 pl-5">
+          <button
+            className="bg-white text-black px-7 py-2 cursor-pointer rounded-md font-light flex justify-center items-center w-full"
+            onClick={async () => {
+              await signIn("credentials", {
+                email: emailRef.current?.value,
+                password: passwordRef.current?.value,
+                callbackUrl: "/"
+              });
+            }}
+          >
+            Login with eamil
+          </button>
+          <div className="flex w-full items-center gap-4 mt-5 mb-5">
+            <div className="flex-grow h-px bg-neutral-600"></div>
+            <span className="text-neutral-500 text-sm">OR CONTINUE WITH</span>
+            <div className="flex-grow h-px bg-neutral-600"></div>
+          </div>
+          <button
+            className="bg-neutral-900 text-white px-7 py-2 cursor-pointer rounded-md font-light flex justify-center items-center w-full mb-4"
+            onClick={ async () => {
+              await signIn("google", {
+                callbackUrl: '/'
+              })
+            }}
+          >
+            Google
+          </button>
+          <button
+            className="bg-neutral-900 text-white px-7 py-2 cursor-pointer rounded-md font-light flex justify-center items-center w-full"
+            onClick={async () => {
+              await signIn("github", {
+                callbackUrl: '/'
+              });
+            }}
+          >
+            GitHub
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
